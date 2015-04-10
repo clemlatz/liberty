0afef78ea1ee53c9ab7907c47dcbb49a9cb61331
Clement Bourgoin <cb@nokto.net>
2015-04-11 01:17:51 +0200

    Handle user connection & disconnection

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-
-// app.get('/', function(req, res){
-//   res.send('<h1>Hello world</h1>');
-// });
+var io = require('socket.io')(http);
 
 app.use(express.static('public'))
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  
+});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
