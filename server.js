117594a5b4e6b3a59abd96777ddc392f1300af81
Clement Bourgoin <cb@nokto.net>
2015-04-11 02:26:34 +0200

    Restart game every 120 seconds

--- server.js
+++ server.js
@@ -5,12 +5,33 @@ var io = require('socket.io')(http);
 var shortid = require('shortid');
 
 // Serve static files
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 function log(msg) {
   var date = new Date();
   console.log(date+' : '+msg);
 }
+
+// Game
+var game = {
+  time: 120, // Time left
+  players: [],
+  timer: null,
+  start: function() {
+    var context = this;
+    this.time = 120;
+    this.timer = setInterval( function() { context.onesec(); }, 1000);
+    log('New game starting');
+  },
+  onesec: function() {
+    this.time -= 1;
+    if (this.time <= 0) {
+      clearInterval(this.timer);
+      this.start();
+    }
+    log('Time left: '+this.time+' sec.');
+  }
+};
 
 // Player
 Player = function(x, y, role) {
@@ -25,22 +46,22 @@ Bot = function(x, y) {
   this.id = shortid.generate();
   this.x = x || 0;
   this.y = y || 0;
-}
+};
 
-var players = [],
-  bots = [];
+// Start game
+game.start();
 
 // Socket IO
 io.on('connection', function(socket) {
   
   // Create a new Player
   socket.player = new Player();
-  players.push(socket.player);
+  game.players.push(socket.player);
   
   log('Player '+socket.player.id+' created');
   
   // Send players list to new player
-  socket.emit('players', players);
+  socket.emit('players', game.players);
   
   // Broadcast new player to all players
   socket.broadcast.emit('player', socket.player);
@@ -49,9 +70,9 @@ io.on('connection', function(socket) {
   socket.on('disconnect', function() {
     
     // Remove player from array
-    var index = players.indexOf(socket.player);
+    var index = game.players.indexOf(socket.player);
     if (index > -1) {
-      players.splice(index, 1);
+      game.players.splice(index, 1);
     }
     
     log('Player '+socket.player.id+' disconnected');
