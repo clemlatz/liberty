468d099a0ee03d3ca8f87230ed2e5dc192f73d6c
Clement Bourgoin <cb@nokto.net>
2015-04-11 02:00:29 +0200

    Create new player on user connect and update player list

--- server.js
+++ server.js
@@ -2,18 +2,63 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var shortid = require('shortid');
 
+// Serve static files
 app.use(express.static('public'))
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+function log(msg) {
+  var date = new Date();
+  console.log(date+' : '+msg);
+}
+
+// Player
+Player = function(x, y, role) {
+  this.id = shortid.generate();
+  this.x = x || 0;
+  this.y = y || 0;
+  this.role = role || 'prisoner'; // prisoner or tower
+};
+
+// Bot
+Bot = function(x, y) {
+  this.id = shortid.generate();
+  this.x = x || 0;
+  this.y = y || 0;
+}
+
+var players = [],
+  bots = [];
+
+// Socket IO
+io.on('connection', function(socket) {
   
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  // Create a new Player
+  socket.player = new Player();
+  players.push(socket.player);
+  
+  log('Player '+socket.player.id+' created');
+  
+  // Send players list to new player
+  socket.emit('players', players);
+  
+  // Broadcast new player to all players
+  socket.broadcast.emit('player', socket.player);
+  
+  // When player disconnect
+  socket.on('disconnect', function() {
+    
+    // Remove player from array
+    var index = players.indexOf(socket.player);
+    if (index > -1) {
+      players.splice(index, 1);
+    }
+    
+    log('Player '+socket.player.id+' disconnected');
   });
   
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  log('Server listening on *:3000');
 });
